2ae31de37125c85392d38cbe614994849c1f7167
mperezi <mperezibars@gmail.com>
2021-03-30 00:46:25 +0200

    Add custom_path

--- src/test_api.py
+++ src/test_api.py
@@ -40,6 +40,46 @@ class TestApi:
         assert r['statusCode'] == 400
         TestApi.assert_body_contains(r, 'message', 'required')
 
+    def test_valid_custom_path(self):
+        with Stubber(s3_client) as stubber:
+            stubber.add_client_error('head_object', service_error_code='404')
+            stubber.add_response('put_object', {})
+            event = {
+                'body': '{"url":"https://www.google.com/", "custom_path": "custom"}'
+            }
+            r = handle(event, context={})
+            body = TestApi.get_body(r)
+            assert r['statusCode'] == 200
+            assert body['path'] == 'custom'
+
+    def test_blank_custom_path(self):
+        with Stubber(s3_client) as stubber:
+            stubber.add_client_error('head_object', service_error_code='404')
+            stubber.add_response('put_object', {})
+            event = {
+                'body': '{"url":"https://www.google.com/", "custom_path": "  "}'
+            }
+            r = handle(event, context={})
+            body = TestApi.get_body(r)
+            assert r['statusCode'] == 200
+            assert len(body['path'].strip()) > 0
+
+    def test_custom_path_already_in_use(self):
+        with Stubber(s3_client) as stubber:
+            stubber.add_response('head_object', {})
+            event = {
+                'body': '{"url":"https://www.google.com/", "custom_path": "custom"}'
+            }
+            r = handle(event, context={})
+            body = TestApi.get_body(r)
+            assert r['statusCode'] == 400
+            assert 'already in use' in body['message']
+
+    @staticmethod
+    def get_body(response):
+        assert 'body' in response
+        return json.loads(response['body'])
+
     @staticmethod
     def assert_body_contains(response, key, match):
         assert 'body' in response
